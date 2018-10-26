5834e9face2bd956fc18d15b1d0f78b8b6441e71
MARKPC\MarkGiambone <giamb103@mail.chapman.edu>
2018-10-26 00:42:53 -0700

    mark bug fixing

--- Green Thumb - From Friday Oct 19/Assets/Scripts/Tower/Tower.cs
+++ Green Thumb - From Friday Oct 19/Assets/Scripts/Tower/Tower.cs
@@ -19,9 +19,19 @@ public class Tower : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        validTargets = GameObject.FindGameObjectsWithTag(targetTag);//put all enemies into array
-        curTarget = null;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        health_text.GetComponent<TextMesh>().text = health.ToString();
+        health_text.transform.LookAt(health_text.transform.position + Camera.main.transform.rotation * Vector3.forward);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        validTargets = GameObject.FindGameObjectsWithTag(targetTag);//put all enemies into array
         if (validTargets.Length != 0)
         {
             for (int i = 0; i < validTargets.Length; i++)
@@ -35,37 +45,11 @@ public class Tower : MonoBehaviour
                 }
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        health_text.GetComponent<TextMesh>().text = health.ToString();
-        health_text.transform.LookAt(health_text.transform.position + Camera.main.transform.rotation * Vector3.forward);
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
 
         if (curTarget == null)
         {
             firing = false;
             rb.freezeRotation = true;
-            //Debug.Log("curtarget = null");
-            validTargets = GameObject.FindGameObjectsWithTag(targetTag);//put all enemies into array
-            if (validTargets.Length != 0)
-            {
-                for (int i = 0; i < validTargets.Length; i++)
-                {//sort array to have closest enemy in array[0]
-                    float dist = Vector3.Distance(transform.position, validTargets[i].transform.position);//calculates distance from tower to enemy
-
-                    if (!curTarget || dist < closestDist)
-                    {
-                        curTarget = validTargets[i];
-                        closestDist = dist;
-                    }
-                }
-            }
         }
 		if (curTarget != null) {
             firing = true;
